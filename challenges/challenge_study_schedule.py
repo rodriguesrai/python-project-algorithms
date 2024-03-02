06b87f0f3c1ba4a64f03383d45b69648339aa7d1
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-03-02 07:34:43 -0300

    req 1 - implementa study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return
 
-# 
+    students = 0
+
+    for entry_start, entry_end in permanence_period:
+        if type(entry_start) is not int or type(entry_end) is not int:
+            return
+        if entry_start <= target_time <= entry_end:
+            students += 1
+
+    return students
