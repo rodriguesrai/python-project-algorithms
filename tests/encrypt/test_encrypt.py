dbe9a03a946659202c80a4fe1fa52ceb649480c1
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-03-02 07:56:15 -0300

    req 2 - implementa test_encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("hello", 2) == "oll_eh"
+
+    with pytest.raises(TypeError):
+        encrypt_message("hello", "2")
+
+    with pytest.raises(TypeError):
+        encrypt_message(123, 2)
+
+    assert encrypt_message("hello", 6) == "olleh"
